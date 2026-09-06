8b3ab663d1968bc2879fe039d8efdb024d892dbe
Pam Worsham <worsham.pj@gmail.com>
2026-09-05 21:45:10 -0400

    Refactor comments in MultiplesOf function and add plan for RotateListRight implementation

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -16,11 +16,11 @@ public static class Arrays
         // ************************************************************
         //       Plan of how to implement the multiplesOf function
         // ************************************************************
-        // 1. create an array that has a length of 5 which is the length given
-        // 2. use a loop to go through the array one by one
-        // 3. multiply the number by each length of the numbers starting with 1 and going until you have reached 5 since 5 is the length
-        // 4. each number needs to be stored in the array
-        // 5. return once the array has completed
+        // create an array that has a length of 5 which is the length given
+        // use a loop to go through the array one by one
+        // multiply the number by each length of the numbers starting with 1 and going until you have reached 5 since 5 is the length
+        // each number needs to be stored in the array
+        // return multiples once the array has completed
 
         double[] multiples = new double[length];
 
@@ -44,5 +44,19 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // *********************************************************************************
+        //                  Plan for solving a complicated problem using a list
+        // **********************************************************************************
+        // start with the list of numbers
+        // find how many numbers need to move from the end to the front using "amount"
+        // save those numbers so they are not lost
+        // remove the saved numbers from their original positions
+        // then insert the saved numbers at the beginning of the list
+
+        int startIndex = data.Count - amount;
+        var numbersToMove = data.GetRange(startIndex, amount);
+        data.RemoveRange(startIndex, amount);
+        data.InsertRange(0, numbersToMove);
     }
 }
